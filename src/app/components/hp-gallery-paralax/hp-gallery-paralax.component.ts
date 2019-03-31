aa620c18fd208577dd3724aff595a487d8a0ab01
Ricard SG <rsimon@mariabarcelona.com>
2019-03-31 20:29:25 +0200

    Hp Gallery Parallax | Improvements: New option to show the step tag.

--- src/app/components/hp-gallery-paralax/hp-gallery-paralax.component.ts
+++ src/app/components/hp-gallery-paralax/hp-gallery-paralax.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, AfterViewInit, SecurityContext } from '@angular/core';
+import { Component, OnInit, Input, SecurityContext } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import * as Rellax from 'rellax';
 
@@ -7,21 +7,20 @@ import * as Rellax from 'rellax';
   templateUrl: './hp-gallery-paralax.component.html',
   styleUrls: ['./hp-gallery-paralax.component.css']
 })
-export class HpGalleryParalaxComponent implements OnInit, AfterViewInit {
+export class HpGalleryParalaxComponent implements OnInit {
 
-  @Input() images: Object[];
+  @Input() gallery: Object[];
+  rellaxClassName: String = '';
 
   constructor(
     private _sanitizer: DomSanitizer
   ) { }
 
   ngOnInit() {
-  }
-
-  ngAfterViewInit(): void {
-    const rellax = new Rellax('.rellax', {
-      center: true
-    });
+    if (this.gallery['rellax'] != undefined && this.gallery['rellax']) {
+      this.rellaxClassName = this.makeid();
+      this.newRellax();
+    }
   }
 
   getFormat(text) {
@@ -38,6 +37,32 @@ export class HpGalleryParalaxComponent implements OnInit, AfterViewInit {
       url = this._sanitizer.bypassSecurityTrustStyle('url(' + url + ')');
     }
     return url;
+  }
+
+  makeid() {
+    const length = 5;
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    if ((<HTMLElement>document.getElementsByClassName('rellax_' + text)[0])) {
+      this.makeid();
+    } else {
+      return 'rellax_' + text;
+    }
+  }
+
+  newRellax() {
+    if ((<HTMLElement>document.getElementsByClassName('' + this.rellaxClassName)[0])) {
+      const rellax = new Rellax('.' + this.rellaxClassName, {
+        center: true
+      });
+    } else {
+      setTimeout(() => {
+        this.newRellax();
+      }, 100);
+    }
   }
 
 }
