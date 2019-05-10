289497adbeb76c8dd9f16c7067d176d0a3155600
Ricard SG <rsimon@mariabarcelona.com>
2019-05-10 18:44:57 +0200

    nuevos textos

--- src/app/layouts/home/hp-sc-reasons/hp-sc-reasons.component.ts
+++ src/app/layouts/home/hp-sc-reasons/hp-sc-reasons.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HpScReasonsComponent implements OnInit {
 
   content = {
-    subtitle: 'La información que necesites para planificar tu viaje.',
-    title: 'Te asesoramos con los tramites de las escuelas y los visados.',
+    title: 'La información que necesites para planificar tu viaje.',
+    // subtitle: 'Te acompañamos en todas las etapas de tu experiencia',
+    subtitle: 'Never alone. Vamos contigo!',
+    // subtitle: 'Te asesoramos con los tramites de las escuelas y los visados.',
     // tslint:disable-next-line:max-line-length
-    paragraph: 'Aprovecha las mejores oportunidades en cada destino gracias a las recomendaciones de nuestro equipo de coaching transformacional.'
+    // paragraph: 'Aprovecha las mejores oportunidades en cada destino gracias a las recomendaciones de nuestro equipo de coaching transformacional.',
+    // tslint:disable-next-line:max-line-length
+    paragraph: 'Te acompañamos en todas las etapas de tu aventura, nosotros ya hemos vivido todas las experiencias. Bienvenido a la comunidad de Worldhoppers.'
   };
 
   // tslint:disable-next-line:no-inferrable-types
--- src/app/layouts/home/hp-sc-services/hp-sc-services.component.ts
+++ src/app/layouts/home/hp-sc-services/hp-sc-services.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HpScServicesComponent implements OnInit {
 
   content = {
-    subtitle: 'SERVICIOS, EVENTOS, PRACTICAS y MUCHO MAS',
+    subtitle: 'PROGRAMAS DE ESTUDIOS, GUIAS, DESCUENTOS y MUCHO MAS',
     title: '¿Qué te ofrecemos?',
     // tslint:disable-next-line:max-line-length
-    paragraph: 'Los mejores programas de estudio a tu alcance en las ciudades de destino para que la experencia que elijas sea perfecta y adaptada a tus necesidades.'
+    paragraph: 'Una experiencia completa. Con los mejores programas de estudio en el extranjero adaptados a tus necesidades. Te asesoramos con los trámites y los visados para que no tengas que preocuparte de nada.'
   };
 
   constructor() { }
