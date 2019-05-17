e009c677f4e724bffac30d3c1486d8f7161c8a97
codeteca <silvia.tormo@gmail.com>
2019-05-17 18:59:44 +0200

    mas merge

--- src/app/layouts/home/hp-sc-services/hp-sc-services.component.ts
+++ src/app/layouts/home/hp-sc-services/hp-sc-services.component.ts
@@ -8,17 +8,10 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HpScServicesComponent implements OnInit {
 
   content = {
-<<<<<<< HEAD
-    subtitle: 'SERVICIOS, EVENTOS, PRACTICAS y MUCHO MAS',
-    title: '¿Qué te ofrecemos?',
-    // tslint:disable-next-line:max-line-length
-    paragraph: 'Los mejores programas de estudio a tu alcance en las ciudades de destino para que la experencia que elijas sea perfecta y adaptada a tus necesidades.'
-=======
     subtitle: 'PROGRAMAS DE ESTUDIOS, GUIAS, DESCUENTOS y MUCHO MAS',
     title: '¿Qué te ofrecemos?',
     // tslint:disable-next-line:max-line-length
     paragraph: 'Una experiencia completa. Con los mejores programas de estudio en el extranjero adaptados a tus necesidades. Te asesoramos con los trámites y los visados para que no tengas que preocuparte de nada.'
->>>>>>> sandra2
   };
 
   constructor() { }
