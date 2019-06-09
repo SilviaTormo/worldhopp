e8cd7bc5adf08a9283da0107d81d7bb1d3af2eb6
codeteca <silvia.tormo@gmail.com>
2019-06-10 00:33:14 +0200

    cambio de fotos en el hopp

--- src/app/layouts/home/hp-sidebar-do-you-lack-a-hopp/hp-sidebar-do-you-lack-a-hopp.component.ts
+++ src/app/layouts/home/hp-sidebar-do-you-lack-a-hopp/hp-sidebar-do-you-lack-a-hopp.component.ts
@@ -12,59 +12,59 @@ export class HpSidebarDoYouLackAHoppComponent implements OnInit, OnDestroy {
 
   cards = [
     {
-      image: '../../../../assets/img/wave-1913559_1280.jpg',
+      image: '../../../../assets/img/mundo.jpg',
       title: 'Verás mundo',
       text: 'Conocerás diferentes formas de vivir, pensar y explorarás nuevos sitios que jamás habías imaginado.'
     },
     {
-      image: '../../../../assets/img/wave-1913559_1280.jpg',
+      image: '../../../../assets/img/cultura.jpg',
       title: 'Entenderás otras culturas',
       // tslint:disable-next-line:max-line-length
       text: 'Ya que vivir en otro país, significa conocer a sus habitantes, relacionarte y sumergirte en sus costumbres y su estilo de vida. Entenderás verdaderamente sus pensamientos y formas de vivir.'
     },
     {
-      image: '../../../../assets/img/wave-1913559_1280.jpg',
+      image: '../../../../assets/img/paisdesdefuera.jpg',
       title: 'Verás tu país desde fuera',
       // tslint:disable-next-line:max-line-length
       text: 'Y aunque te parezca raro, te darás cuenta de que tu propio país tiene cosas estupendas y que hay otras que podrían mejorarse bastante. Serás mucho mas objetivo.'
     },
     {
-      image: '../../../../assets/img/wave-1913559_1280.jpg',
+      image: '../../../../assets/img/amigos.jpg',
       title: 'Amigos para toda la vida',
       // tslint:disable-next-line:max-line-length
       text: 'Conocerás gente de todas partes del mundo, compartirás experiencias inolvidables, momentos difíciles y horas de charlas, y seréis amigos para siempre por que al cruzar vuestros caminos todo fue diferente. Además a partir de este momento, seguro que ya no tienes excusas para ir a visitar a tus nuevos amigos en sus países, crearás una red de amistades internacional.'
     },
     {
-      image: '../../../../assets/img/wave-1913559_1280.jpg',
+      image: '../../../../assets/img/adventure.jpg',
       title: 'Romperás con tu rutina',
       // tslint:disable-next-line:max-line-length
       text: 'Romperás tanto con tu rutina que será como una segunda vida, una oportunidad de hacer las cosas diferentes. Tendás tiempo para ti y para hacer lo que más te guste, ¡reinvéntate!'
     },
     {
-      image: '../../../../assets/img/wave-1913559_1280.jpg',
+      image: '../../../../assets/img/hablar.jpg',
       title: 'El placer de hablar otro idioma',
       // tslint:disable-next-line:max-line-length
       text: 'Porque cuando al principio la comunicación se hace más compleja, con los días adquirirás más fluidez, vocabulario y sobre todo confianza y seguridad en ti mismo, y empezarás a entender las canciones que has cantado toda tu vida en plan wachu wachu!'
     },
     {
-      image: '../../../../assets/img/wave-1913559_1280.jpg',
+      image: '../../../../assets/img/viajar.jpg',
       title: 'Viajas',
       // tslint:disable-next-line:max-line-length
       text: 'Te animarás a hacer muchísimas salidas entre semana y escapadas de fin de semana, puentes, vacaciones, estarás muy activo porque vas a querer aprovechar al máximo esta experiencia y tu memoria se llenará de momentos inolvidables.'
     },
     {
-      image: '../../../../assets/img/wave-1913559_1280.jpg',
+      image: '../../../../assets/img/curriculum.jpg',
       title: 'Mejorarás tu CV y tus oportunidades laborales',
       // tslint:disable-next-line:max-line-length
       text: 'Porque tu perspectiva será más amplia, tu experiencia será diferente, confiaras mas en ti mismo y los cursos que realices darán a tu hoja de vida un valor mas agregado.'
     },
     {
-      image: '../../../../assets/img/wave-1913559_1280.jpg',
-      title: 'Enriquecerás tu vida!',
+      image: '../../../../assets/img/enriqueceras.jpg',
+      title: '¡Enriquecerás tu vida!',
       text: 'E inspirarás a la gente que te rodea; a tus amigos, hermanos, familia a perseguir sus sueños y dar el salto.'
     },
     {
-      image: '../../../../assets/img/wave-1913559_1280.jpg',
+      image: '../../../../assets/img/feliz.jpg',
       title: '¡Serás feliz!',
       // tslint:disable-next-line:max-line-length
       text: 'Y cuando cierres la etapa y mires para atrás, sabrás que fue lo correcto porque fue lo que querías hacer, con sus momentos duros y sus momentos de risas.'
